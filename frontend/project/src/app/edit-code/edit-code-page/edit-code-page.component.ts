a1b15f62aed1a59143546c30a67e48e4901a4913
lyubolp <lyubo.karev@gmail.com>
2021-09-25 11:03:32 +0300

    Addedd a page with the monaco editor

---
+++ frontend/project/src/app/edit-code/edit-code-page/edit-code-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-code-page',
+  templateUrl: './edit-code-page.component.html',
+  styleUrls: ['./edit-code-page.component.scss']
+})
+export class EditCodePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
