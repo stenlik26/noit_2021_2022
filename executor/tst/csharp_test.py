371a7aeec4470359d1707223845f9045d49dbeb8
lyubolp <lyubo.karev@gmail.com>
2021-12-23 22:20:22 +0200

    Added CSharp tests

--- executor/tst/csharp_test.py
+++ executor/tst/csharp_test.py
@@ -1,64 +1,68 @@
 import os.path
+import subprocess
 import unittest
 
 from typing import List
 from src.config import Config
-from src.cpp import CppLanguage
+from src.csharp import CsharpLanguage
 
 
-class CppTest(unittest.TestCase):
+class CsharpTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
-        cls.compiler = '/usr/bin/g++'
-        cls.extension = '.cpp'
+        cls.compiler = '/usr/bin/dotnet'
+        cls.extension = '.cs'
         cls.is_compiled = True
-        cls.linter = '/usr/bin/clang-tidy'
-        cls.config = Config()
+        cls.linter = ''
+        cls.config = Config(os.path.join('/tmp', 'cs'))
 
         if not os.path.exists(cls.config.work_dir_root):
             os.makedirs(cls.config.work_dir_root, exist_ok=True)
 
         cls.valid_code_lines = [
-            '#include <iostream>\n',
+            'using System;\n',
             '\n',
-            'int main() {\n',
-            '\t//Program that prints "Hello world" to the console\n',
-            '\tstd::cout<<"Hello world"<<std::endl;\n',
-            '\treturn 0;\n',
+            'namespace cs\n',
+            '{\n',
+            '\tclass Program\n',
+            '\t{\n',
+            '\t\tstatic void Main(string[] args)\n',
+            '\t\t{\n',
+            '\t\t\tConsole.WriteLine("Hello world");\n',
+            '\t\t}\n',
+            '\t}\n',
             '}\n',
         ]
 
         cls.invalid_code_lines = [
-                '#include <iostream>\n',
-                '\n',
-                'int main() {\n',
-                '\t//Program that prints "Hello world" to the console\n',
-                '\tstd::cout<<"Hello world"<<std::endl\n',
-                '\treturn 0;\n',
-                '}\n'
-            ]
+            'using System;\n',
+            '\n',
+            'namespace cs\n',
+            '{\n',
+            '\tclass Program\n',
+            '\t\tstatic void Main(string[] args)\n',
+            '\t\t{\n',
+            '\t\t\tConsole.WriteLine("Hello world")\n',
+            '\t\t}\n',
+            '\t}\n',
+            '}\n',
+        ]
 
-        cls.tmp_cpp_file_path = os.path.join('/tmp', 'sample.cpp')
-        cls.cpp = CppLanguage(cls.compiler, cls.extension, cls.is_compiled,
-                              cls.linter)
+        subprocess.run(['dotnet', 'new', 'console', '-o', cls.config.work_dir_root])
+
+        cls.tmp_cs_file_path = os.path.join(cls.config.work_dir_root, 'Program.cs')
+        cls.csharp = CsharpLanguage(cls.compiler, cls.extension, cls.is_compiled,
+                                    cls.linter)
 
     @classmethod
     def tearDownClass(cls) -> None:
         if os.path.exists(cls.config.work_dir_root):
-            for dirname, dirs, files in os.walk(cls.config.work_dir_root):
-                for file in files:
-                    os.remove(os.path.join(dirname, file ))
-
-            os.rmdir(cls.config.work_dir_root)
-
-    def tearDown(self) -> None:
-        if os.path.exists(self.tmp_cpp_file_path):
-            os.remove(self.tmp_cpp_file_path)
+            subprocess.run(["rm", "-rf" , f"{cls.config.work_dir_root}"])
 
     def test_01_compile_valid_code(self):
         self.__write_to_tmp_file(self.valid_code_lines)
 
-        result = self.cpp.compile(self.tmp_cpp_file_path)
+        result = self.csharp.compile(self.config.work_dir_root)
 
         self.assertEqual(result[1], "")
         self.assertEqual(result[2], 0)
@@ -66,41 +70,44 @@ class CppTest(unittest.TestCase):
     def test_02_compile_invalid_code(self):
         self.__write_to_tmp_file(self.invalid_code_lines)
 
-        result = self.cpp.compile(self.tmp_cpp_file_path)
+        result = self.csharp.compile(self.config.work_dir_root)
 
-        self.assertNotEqual(result[1], "")
+        self.assertNotEqual(result[0], "")
         self.assertNotEqual(result[2], 0)
 
     def test_03_execute_valid_code_no_stdin(self):
         self.__write_to_tmp_file(self.valid_code_lines)
 
-        result = self.cpp.execute(self.tmp_cpp_file_path)
+        result = self.csharp.execute(self.config.work_dir_root)
 
-        self.assertEqual(result[0], "Hello world\n")
+        self.assertEqual(result[0].strip(), "Hello world")
         self.assertEqual(result[2], 0)
 
     def test_04_execute_valid_code_with_stdin(self):
-        cpp_lines = [
-            '#include <iostream>\n',
+        csharp_lines = [
+            'using System;\n',
             '\n',
-            'int main() {\n',
-            '\t//Program that prints "Hello world" to the console\n',
-            '\tint a = 0;\n',
-            '\tstd::cin >> a;\n',
-            '\tint b = 0;\n',
-            '\tstd::cin >> b;\n',
-            '\tstd::cout<<a+b<<std::endl;\n',
-            '\treturn 0;\n',
+            'namespace cs\n',
+            '{\n',
+            '\tclass Program\n',
+            '\t{\n',
+            '\t\tstatic void Main(string[] args)\n',
+            '\t\t{\n',
+            '\t\t\tint a = Convert.ToInt32( Console.ReadLine());\n'
+            '\t\t\tint b = Convert.ToInt32( Console.ReadLine());\n'
+            '\t\t\tConsole.WriteLine(a+b);\n',
+            '\t\t}\n',
+            '\t}\n',
             '}\n',
         ]
-        self.__write_to_tmp_file(cpp_lines)
+        self.__write_to_tmp_file(csharp_lines)
 
-        stdin_content = "3\n5\n";
+        stdin_content = "3\n5\n"
         stdin_path = os.path.join("/tmp", "test_03_stdin.txt")
         with open(stdin_path, 'w+') as fp:
             fp.write(stdin_content)
 
-        result = self.cpp.execute(self.tmp_cpp_file_path, stdin_path)
+        result = self.csharp.execute(self.config.work_dir_root, stdin_path)
 
         os.remove(stdin_path)
         self.assertEqual(result[0], "8\n")
@@ -109,22 +116,24 @@ class CppTest(unittest.TestCase):
     def test_05_execute_invalid(self):
         self.__write_to_tmp_file(self.invalid_code_lines)
 
-        result = self.cpp.execute(self.tmp_cpp_file_path)
+        result = self.csharp.execute(self.config.work_dir_root)
 
-        self.assertNotEqual(result[1], "")
+        self.assertNotEqual(result[0], "")
         self.assertNotEqual(result[2], 0)
 
     def test_06_lint_valid_code(self):
         self.__write_to_tmp_file(self.valid_code_lines)
 
-        result = self.cpp.lint(self.tmp_cpp_file_path)
+        result = self.csharp.lint(self.config.work_dir_root)
 
-        print(result[0])
         self.assertEqual(result[2], 0)
 
     def __write_to_tmp_file(self, content: List[str]):
-        with open(self.tmp_cpp_file_path, 'w+') as fp:
+        with open(self.tmp_cs_file_path, 'w+') as fp:
             fp.writelines(content)
+
+    def __setup_csharp_project(self):
+        subprocess.run(['dotnet', 'new', 'console', '-o', self.config.work_dir_root])
 
 
 if __name__ == '__main__':
