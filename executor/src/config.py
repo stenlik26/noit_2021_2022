b89634af4ad9c1fdc89df49f1fb550db0d34e79b
lyubolp <lyubo.karev@gmail.com>
2021-12-23 22:19:43 +0200

    Changed config.py so that it accepts a parameter

--- executor/src/config.py
+++ executor/src/config.py
@@ -1,8 +1,8 @@
 # TODO - Make singleton
 # TODO - Create this based on DB value
 class Config:
-    def __init__(self):
-        self.__work_dir_root = "/tmp/test"
+    def __init__(self, work_dir_root="/tmp/test"):
+        self.__work_dir_root = work_dir_root
 
     @property
     def work_dir_root(self) -> str:
