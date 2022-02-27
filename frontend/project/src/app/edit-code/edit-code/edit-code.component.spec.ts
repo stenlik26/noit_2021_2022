bf7fef0828790ba1cc0b494682cbbc640a597735
lyubolp <lyubo.karev@gmail.com>
2022-02-27 20:50:15 +0200

    Added write code page

---
+++ frontend/project/src/app/edit-code/edit-code/edit-code.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EditCodeComponent } from './edit-code.component';
+
+describe('EditCodeComponent', () => {
+  let component: EditCodeComponent;
+  let fixture: ComponentFixture<EditCodeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ EditCodeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EditCodeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
