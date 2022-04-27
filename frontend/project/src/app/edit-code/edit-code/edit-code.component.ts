31de82169ceff11ce90eb677ace2d446d5500e17
unknown <stenli_karev@abv.bg>
2022-04-27 17:43:06 +0300

    Download code button done.

--- frontend/project/src/app/edit-code/edit-code/edit-code.component.ts
+++ frontend/project/src/app/edit-code/edit-code/edit-code.component.ts
@@ -124,6 +124,56 @@ export class EditCodeComponent implements OnInit {
     return problem_information;
   }
 
+  download_code()
+  {
+    //@ts-ignore
+    const content = this.editor.getValue();
+
+    const filename_field: HTMLInputElement = document.getElementById("filename_input") as HTMLInputElement;
+    let filename = filename_field.value;
+
+    switch(this.getLanguageFromSelect())
+    {
+      case "cpp":
+        {
+          filename += ".cpp";
+          break;
+        }
+      case "java":{
+        filename += ".java";
+        break;
+      }
+      case "python":{
+        filename += ".py";
+        break;
+      }
+    }
+
+
+    const element = document.createElement('a');
+    
+    //A blob is a data type that can store binary data
+    // "type" is a MIME type
+    // It can have a different value, based on a file you want to save
+    const blob = new Blob([content], { type: 'plain/text' });
+
+    //createObjectURL() static method creates a DOMString containing a URL representing the object given in the parameter.
+    const fileUrl = URL.createObjectURL(blob);
+    
+    //setAttribute() Sets the value of an attribute on the specified element.
+    element.setAttribute('href', fileUrl); //file location
+    element.setAttribute('download', filename); // file name
+    element.style.display = 'none';
+    
+    //use appendChild() method to move an element from one element to another
+    document.body.appendChild(element);
+    element.click();
+    
+    //The removeChild() method of the Node interface removes a child node from the DOM and returns the removed node
+    document.body.removeChild(element);
+  }
+
+
   upload_code_output(json: any)
   {
     this.showToast("Вашето решение е качено успешно.");
